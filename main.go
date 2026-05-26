539aa665698bf736b13dafe0170a3aadc4e30ea2
smallscaleserver <smallscaleserver@gmail.com>
2026-05-26 13:39:49 +0700

    Update main.go to implement a+b+c = ? functionality

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	var a, b, c int
+	fmt.Print("Enter first number: ")
+	fmt.Scanf("%d", &a)
+	fmt.Print("Enter second number: ")
+	fmt.Scanf("%d", &b)
+	fmt.Print("Enter third number: ")
+	fmt.Scanf("%d", &c)
+
+	result := a + b + c
+	fmt.Printf("Result: %d + %d + %d = %d\n", a, b, c, result)
 }
